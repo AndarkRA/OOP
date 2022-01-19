eab2dd83c9597f19e5282498142afc10b5722852
AndarkRA <klushin1973@yandex.ru>
2022-01-19 23:22:03 +0300

    task 04

---
+++ Lesson 02 task 04/Lesson 02 task 04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bankAccount = new BankAccount(AccountType.Current, 250);
+			bankAccount.AccType = AccountType.Payments;
+			bankAccount.Balance += 100;
+
+			Console.WriteLine(bankAccount.Print());
+		}
+	}
+}
