c97f92a925488cff19eb2845afca9af6fee87327
AndarkRA <klushin1973@yandex.ru>
2022-01-24 17:32:00 +0300

    Первое задание

---
+++ Lesson 02 task 01/Lesson 02 task 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var bankAcc1 = new BankAccount(AccountType.Current, 500);
+			var bankAcc2 = new BankAccount(AccountType.Current, 230);
+
+			if (bankAcc1.MoneyTransfer(bankAcc2, 25))
+				Console.WriteLine("Успешный перевод денег");
+			else
+				Console.WriteLine("Перевод денег не удался");
+		}
+	}
+}
